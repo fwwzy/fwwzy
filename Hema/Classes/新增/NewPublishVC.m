f333b0d7fad80fb0b870c0d8fc844aba10a8a34c
abcacbabcacb1 <297686728@qq.com>
2015-12-23 13:35:57 +0800

    cell1

--- Hema/Classes/新增/NewPublishVC.m
+++ Hema/Classes/新增/NewPublishVC.m
@@ -14,9 +14,27 @@
 
 @implementation NewPublishVC
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)loadSet {
+    [self.navigationItem setNewTitle:@"最新揭晓"];
+}
+
+- (void)loadData {
     
 }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
