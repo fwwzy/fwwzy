dfd7cab24b7a88b8c79250a085ad885d7988aaec
mstail <472601115@qq.com>
2015-12-23 11:40:32 +0800

    修改

---
+++ Hema/Classes/新增/HomePageVC.m
@@ -0,0 +1,37 @@
+//
+//  HomePageVC.m
+//  Hema
+//
+//  Created by MsTail on 15/12/23.
+//  Copyright © 2015年 Hemaapp. All rights reserved.
+//
+
+#import "HomePageVC.h"
+
+@interface HomePageVC ()
+
+@end
+
+@implementation HomePageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
