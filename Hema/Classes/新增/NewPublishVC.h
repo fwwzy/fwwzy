da72de8b48b06d67322befaf578a02d0b4ba2a60
abcacbabcacb1 <297686728@qq.com>
2015-12-23 11:43:41 +0800

    NewPublishVC

---
+++ Hema/Classes/新增/NewPublishVC.h
@@ -0,0 +1,13 @@
+//
+//  NewPublishVC.h
+//  Hema
+//
+//  Created by Lsy on 15/12/23.
+//  Copyright © 2015年 Hemaapp. All rights reserved.
+//
+
+#import "PullRefreshGroupVC.h"
+
+@interface NewPublishVC : PullRefreshGroupVC
+
+@end
---
+++ Hema/Classes/新增/NewPublishVC.m
@@ -0,0 +1,37 @@
+//
+//  NewPublishVC.m
+//  Hema
+//
+//  Created by Lsy on 15/12/23.
+//  Copyright © 2015年 Hemaapp. All rights reserved.
+//
+
+#import "NewPublishVC.h"
+
+@interface NewPublishVC ()
+
+@end
+
+@implementation NewPublishVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
