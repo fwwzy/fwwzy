ed926d5d5d8500e2695ef5d181565884d0091435
abcacbabcacb1 <297686728@qq.com>
2015-12-23 13:25:29 +0800

    cell

---
+++ Hema/Classes/新增/NewPublishCell.h
@@ -0,0 +1,22 @@
+//
+//  NewPublishCell.h
+//  Hema
+//
+//  Created by Lsy on 15/12/23.
+//  Copyright © 2015年 Hemaapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPublishCell : UICollectionViewCell
+@property(nonatomic,strong)UIImageView *iconView;
+@property(nonatomic,strong)UILabel *nameLabel;
+@property(nonatomic,strong)UILabel *titleLabel;
+@property(nonatomic,strong)UILabel *priceLabel;
+@property(nonatomic,strong)UILabel *numLabel;
+@property(nonatomic,strong)UILabel *winerLabel;
+@property(nonatomic,strong)UILabel *phoneLabel;
+@property(nonatomic,strong)UIImageView *winView;
+@property(nonatomic,strong)UILabel *curLabel;
+@property(nonatomic,strong)UILabel *timeLabel;
+@end
