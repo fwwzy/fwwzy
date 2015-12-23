cd4d012e809742bbc39f5a2a0f6f4cb4131cb9a7
mstail <472601115@qq.com>
2015-12-23 11:25:06 +0800

    添加HomePage

---
+++ HomePageVC.h
@@ -0,0 +1,13 @@
+//
+//  HomePageVC.h
+//  Hema
+//
+//  Created by MsTail on 15/12/23.
+//  Copyright © 2015年 Hemaapp. All rights reserved.
+//
+
+#import "PullRefreshGroupVC.h"
+
+@interface HomePageVC : PullRefreshGroupVC
+
+@end
